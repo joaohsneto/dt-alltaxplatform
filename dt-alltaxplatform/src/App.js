12b3f3917d53bde574c47d8b5b3cf67f67ba1279
João Herculano de Souza Neto <joaohsneto@gmail.com>
2022-02-01 07:36:25 -0300

    Iniciando o Desafio Técnico

---
+++ dt-alltaxplatform/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import SalesGraphics from './components/SalesGraphic';
+
+function App() {
+  return (
+    <>
+      <SalesGraphics />
+    </>
+  );
+}
+
+export default App;
---
+++ dt-alltaxplatform/src/components/SalesGraphic.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SalesGraphics = () => {
+  return (
+    <>
+      <h1> Iniciando o Desafio Técnico</h1>
+    </>
+  )
+}
+
+export default SalesGraphics;
